cefe93e07e01da310fbd14f757929640b5a6bef5
Martin Huter <martin.huter@splendit.at>
2018-10-02 16:45:35 +0200

    ADD maven modules

---
+++ maven.proguard.executer/src/main/java/at/splendit/executer/App.java
@@ -0,0 +1,16 @@
+package at.splendit.executer;
+
+import at.splendit.core.ClassNameUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Multi-maven-proguard showcase" );
+        System.out.println( ClassNameUtil.returnClassName() );
+    }
+}
---
+++ maven.proguard.executer/src/test/java/at/splendit/showcase/executer/AppTest.java
@@ -0,0 +1,16 @@
+package at.splendit.showcase.executer;
+
+import org.junit.Test;
+
+import at.splendit.executer.App;
+import junit.framework.TestCase;
+
+public class AppTest 
+    extends TestCase
+{
+	@Test
+    public void testMain()
+    {
+        App.main(null);
+    }
+}
