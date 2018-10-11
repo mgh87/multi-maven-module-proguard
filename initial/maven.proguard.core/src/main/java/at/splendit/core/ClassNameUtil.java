978f3f71503993fe97c0fbc59b592d530832f3b0
Martin Huter <e0727981@student.tuwien.ac.at>
2018-10-11 08:43:03 +0200

    update initial with execution plugin

--- initial/maven.proguard.core/src/main/java/at/splendit/core/ClassNameUtil.java
+++ initial/maven.proguard.core/src/main/java/at/splendit/core/ClassNameUtil.java
@@ -10,5 +10,13 @@ public class ClassNameUtil
 	public static final String returnClassName() {
 		return ClassNameUtil.class.getName();
 	}
+	
+	public static final String ofuscatedMethodWrapper() {
+		return obfuscatedMethod();
+	}
+	
+	private static final String obfuscatedMethod() {
+		return Thread.currentThread().getStackTrace()[1].getMethodName();
+	}
 
 }
