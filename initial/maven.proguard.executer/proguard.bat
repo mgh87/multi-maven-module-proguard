6b6555c61aa184297cf87a3d55ed4e71df4659d1
mgh87 <martin.huter@splendit.at>
2018-10-18 11:11:57 +0200

    reference correct artifact

--- initial/maven.proguard.executer/proguard.bat
+++ initial/maven.proguard.executer/proguard.bat
@@ -1,6 +1,6 @@
 java -classpath ^
-..\maven.proguard.core\target\maven.proguard.core-0.2.0-SNAPSHOT-pg.jar;^
-target\maven.proguard.executer-0.2.0-SNAPSHOT-pg.jar;^
+..\maven.proguard.core\target\maven.proguard.core-0.1.0-pg.jar;^
+target\maven.proguard.executer-0.1.0-pg.jar;^
 target\lib\slf4j-api-1.7.25.jar;^
 target\lib\logback-classic-1.2.3.jar;^
 target\lib\logback-core-1.2.3.jar^
