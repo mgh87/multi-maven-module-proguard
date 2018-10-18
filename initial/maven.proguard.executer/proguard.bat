864c284e4ea7a90f271a1dfcfe2ef1029ee26e7c
mgh87 <martin.huter@splendit.at>
2018-10-18 09:17:33 +0200

    added support for windows for proguard execution

---
+++ initial/maven.proguard.executer/proguard.bat
@@ -0,0 +1,7 @@
+java -classpath ^
+..\maven.proguard.core\target\maven.proguard.core-0.2.0-SNAPSHOT-pg.jar;^
+target\maven.proguard.executer-0.2.0-SNAPSHOT-pg.jar;^
+target\lib\slf4j-api-1.7.25.jar;^
+target\lib\logback-classic-1.2.3.jar;^
+target\lib\logback-core-1.2.3.jar^
+ at.splendit.executer.App
